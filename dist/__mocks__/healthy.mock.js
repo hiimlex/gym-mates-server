24c9536d78ff05fa76df0d53c048d14a4433616c
Alex Rodrigues <alex.adaumi@gmail.com>
2025-08-29 13:37:34 -0300

    removing dist from main branch

--- dist/__mocks__/healthy.mock.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.create_healthy_mock = void 0;
-const faker_1 = require("@faker-js/faker");
-const create_healthy_mock = (healthy) => (Object.assign({ body_fat: faker_1.faker.number.int({ min: 10, max: 30 }), weight: faker_1.faker.number.int({ min: 50, max: 100 }), height: faker_1.faker.number.int({ min: 150, max: 200 }) }, healthy));
-exports.create_healthy_mock = create_healthy_mock;
--- dist/types/generics/constants.model.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MongoDBUri = void 0;
-exports.MongoDBUri = process.env.DB_URL || "";
